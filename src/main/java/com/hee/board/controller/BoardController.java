3a7e576c9574f11298ae68dd69c140bbbf3c967d
chlvhksl <hwan4670@naver.com>
2022-10-05 01:56:19 +0900

    글 목록, 상세, 삭제, 수정 처리

--- src/main/java/com/hee/board/controller/BoardController.java
+++ src/main/java/com/hee/board/controller/BoardController.java
@@ -4,7 +4,9 @@ import com.hee.board.entity.Board;
 import com.hee.board.service.BoardService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 
@@ -16,7 +18,7 @@ public class BoardController {
 
     @GetMapping("/board/write") //localhost:8080/board/write
     public String boardWriteForm(){
-        return "boardwrite";
+        return "boardWrite";
     }
 
     @PostMapping("/board/writepro")
@@ -25,4 +27,38 @@ public class BoardController {
         return "";
     }
 
+    @GetMapping("/board/list")
+    public String boardList(Model model){
+        model.addAttribute("list", boardService.boardList());//model 'list'에 게시글 목록을 담아서 반환시킴
+        return "boardList";
+    }
+
+    @GetMapping("/board/detail") //localhost:8080/board/detail?id=1
+    public String boardDetail(Model model, Integer id){
+        model.addAttribute("board", boardService.boardDetail(id));
+        return "boardDetail";
+    }
+
+    @GetMapping("/board/delete")
+    public String boardDelete(Integer id){
+        boardService.boardDelete(id);
+        return "redirect:/board/list";
+    }
+
+    @GetMapping("/board/modify/{id}") //@PathVariable - url에 변수로 사용됨
+    public String boardModify(@PathVariable("id") Integer id,  Model model){
+        model.addAttribute("board", boardService.boardDetail(id));
+        return "boardModify";
+    }
+
+    @PostMapping("/board/update/{id}")
+    public String boardUpdate(@PathVariable("id") Integer id, Board board){
+        Board boardTemp = boardService.boardDetail(id);
+        boardTemp.setTitle(board.getTitle());
+        boardTemp.setContent(board.getContent());
+
+        boardService.write(boardTemp);//사실 덮어 씌우면 안된다고한다...
+
+        return "redirect:/board/list";
+    }
 }
--- src/main/java/com/hee/board/service/BoardService.java
+++ src/main/java/com/hee/board/service/BoardService.java
@@ -5,13 +5,32 @@ import com.hee.board.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class BoardService {
 
     @Autowired
     private BoardRepository  boardRepository;
 
+    //글 작성
     public void write(Board board){
         boardRepository.save(board);
     }
+
+    //게시글 목록
+    public List<Board> boardList(){
+        return boardRepository.findAll();
+    }
+
+    //게시글 상세
+    public Board boardDetail(Integer id){
+        return boardRepository.findById(id).get();
+    }
+
+    //특정 게시글 삭제
+    public void boardDelete(Integer id){
+        boardRepository.deleteById(id);
+    }
+
 }
